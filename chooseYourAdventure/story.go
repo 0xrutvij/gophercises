1200a02394212381d841d595c80be89642e45713
0xrutvij <54966871+0xrutvij@users.noreply.github.com>
2022-01-12 20:23:21 -0600

    Added Functional Options for custom paths and template.

--- chooseYourAdventure/story.go
+++ chooseYourAdventure/story.go
@@ -39,26 +39,52 @@ func JsonStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
-}
+type HandlerOption func(h *handler)
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 
-	path = path[1:]
+	return path[1:]
+}
+
+func WithPathFunction(pf func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = pf
+	}
+}
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, page_template, defaultPathFn}
+
+	for _, opt := range opts {
+		opt(&h)
+	}
+
+	return h
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
-		err := page_template.Execute(w, chapter)
+		err := h.t.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
